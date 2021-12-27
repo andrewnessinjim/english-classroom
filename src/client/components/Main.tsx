00befd0e65284c69d4a447e2ab95ec90f89174fa
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-27 09:39:42 +0530

    Header, main, footer skeleton

---
+++ src/client/components/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+    return (
+        <main className="main">
+            <h1>Main Heading</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio magnam officiis deserunt laboriosam? Quibusdam molestiae, necessitatibus accusamus amet, recusandae magnam sint magni ratione illo tempora, hic quis odit quia voluptatum!</p>
+        </main>
+    )
+}
+
+export default Main;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -9,73 +9,21 @@ import {
     InMemoryCache
 } from "@apollo/client";
 
-import { useQuery, gql } from "@apollo/client";
-import images from "./images/loadImages";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-const HOMEPAGE_QUERY = gql`
-query {
-  homePage {
-    heading
-    toolSets {
-      heading
-      tools {
-        logoKey
-        toolTip
-        logoHeightPx
-      }
-    }
-  }
-}
-`
+
 const client = new ApolloClient({
     uri: '/graphql',
     cache: new InMemoryCache()
 })
 
-const HomePage = () => {
-    const {data} = useQuery(HOMEPAGE_QUERY);
-    return (
-        <main>{data && (
-            <>
-                <Heading heading={data.homePage.heading}/>
-                <div className="tool-sets-container">
-                    {data.homePage.toolSets
-                    .map((toolSet, index) => 
-                        <ToolSet
-                            key={index}
-                            heading={toolSet.heading}
-                            tools={toolSet.tools}/>
-                    )}
-                </div>
-            </>
-        )}
-        </main>)
-}
-const Heading = (props) => {
-    return <h1>{props.heading}</h1>
-}
-
-const ToolSet = (props) => {
-    return (
-        <section className="tool-set-container">
-            <h2>{props.heading}</h2>
-            <div className="logos-container">
-                {props.tools.map((tool, index) =>
-                    <img 
-                        key={index}
-                        src={images[tool.logoKey]}
-                        height={tool.logoHeightPx? tool.logoHeightPx+"px": "32px"}
-                        className="logo"/>)
-                }
-            </div>
-            
-        </section>
-    );
-}
-
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <HomePage />
+        <Header/>
+        <Main/>
+        <Footer/>
     </ApolloProvider>,
     document.getElementById("mount")
 );
