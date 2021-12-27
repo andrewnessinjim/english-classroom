f34358daa07752f4d99af2ff155a4ca8bdf2c4e4
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-27 10:29:10 +0530

    Fix test case description

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress"/>
 
-it('displays hello world', () => {
+it('displays site heading', () => {
     cy.visit('/');
     cy.get('.site-heading')
         .should('contain', 'Sarah\'s Classroom');
