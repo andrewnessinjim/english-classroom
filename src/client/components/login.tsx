91531952c465ca4cf03e82cc0d6ac06b2e0fd5ae
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-29 10:01:50 +0530

    Client side login functionality

--- src/client/components/login.tsx
+++ src/client/components/login.tsx
@@ -1,18 +1,67 @@
 import "./login.scss"
 
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
+const LOGIN_OP = gql`
+mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    token
+    user {
+      id
+      role
+    }
+  }
+}
+`
 function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, {data, loading, error}] = useMutation(LOGIN_OP);
+    const [isTyping, setIsTyping] = useState(false);
+    const navigate = useNavigate();
+
+    if(data && data.login && data.login.token) {
+        navigate("/teachers/sarah");
+    }
+
     return (
         <section className="login-container">
-            <div className="login-form">
-                <input type="text" placeholder="Username"/>
-                <input type="password" placeholder="Password"/>
-                <button>Login</button>
-            </div>
+            <form className="login-form">
+                <input type="text" placeholder="Username" value={username} onChange={e => handleUsernameChange(e.target.value)}/>
+                <input type="password" placeholder="Password" value={password} onChange={e => handlePasswordChange(e.target.value)}/>
+                <button onClick={handleLogin}>Login</button>
+                <p className={`error ${error && !isTyping? "visible": "invisible"}`}>
+                    {`${error? error.message :"&nbsp;"}`}
+                </p>
+            </form>
         </section>
     )
+
+    function handleUsernameChange(username){
+        setIsTyping(true);
+        setUsername(username);
+    }
+
+    function handlePasswordChange(password) {
+        setIsTyping(true);
+        setPassword(password);
+    }
+
+    function handleLogin(e) {
+        e.preventDefault();
+        setIsTyping(false);
+
+        if(loading) return;
+        login({
+            variables: {
+                username,
+                password
+            }
+        })
+    }
 }
+
 
 export default Login;
