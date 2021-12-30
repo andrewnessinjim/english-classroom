492511af87713e0539bc0587c75fe31a4377e924
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-30 14:45:16 +0530

    Student addition form

--- src/client/components/studentsList.tsx
+++ src/client/components/studentsList.tsx
@@ -18,12 +18,26 @@ function StudentsList(props) {
     const {data, loading, error} = useQuery(FETCH_STUDENTS_OP, {
         variables: {teacherId: user.id}
     });
+    let [isAddStudentModalOpen, setAddStudentModelOpen] = useState(false);
+    let [newStudentName, setNewStudentName] = useState("");
     return (
         !user.token?
             <Navigate to="/"/> 
             :
             <section className="students">
-                <h2 className="students--heading">My Students</h2>
+                {!isAddStudentModalOpen ? "" :<section className="modal-overlay" onClick={() => setAddStudentModelOpen(false)}>
+                    <section className="modal-container" onClick={e => e.stopPropagation()}>
+                        <form className="modal-content">
+                            <h3>Add Student</h3>
+                            <input type="text" placeholder="Name" minLength={3} required value={newStudentName} onChange={(e) => setNewStudentName(e.target.value)}/>
+                            <button className="btn addStudent-btn" onClick={handleStudentAddition}>Add</button>
+                        </form>
+                    </section>
+                </section>}
+                <section className="students-header">
+                    <h2>My Students</h2>
+                    <button className="btn" onClick={() => setAddStudentModelOpen(true)}>Add Student</button>
+                </section>
                 <ol className="students--list">
                     {data && data.fetchStudents.map(student => 
                         <li className="student" key = {student._id}>
@@ -38,6 +52,12 @@ function StudentsList(props) {
                 </ol>
             </section>
     )
+
+    function handleStudentAddition(e) {
+        e.preventDefault();
+
+        //TODO: Send request to server to add student
+    }
 }
 
 export default StudentsList;
