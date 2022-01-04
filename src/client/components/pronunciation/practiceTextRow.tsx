ad6b9d1e02bd9bf947783a756959cbb6367081fb
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-01-04 19:20:06 +0530

    Display multi-line text without truncating

--- src/client/components/pronunciation/practiceTextRow.tsx
+++ src/client/components/pronunciation/practiceTextRow.tsx
@@ -1,19 +1,25 @@
 import "./practiceTextRow.scss";
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import RatingScale from "./ratingScale";
+import autosize from "autosize";
 
 function PracticeTextRow(props) {
     const [text, setText] = useState(props.text);
-
+    let textArea;
+    useEffect(() => {
+        if(textArea) autosize(textArea);
+    })
     return (
         <li className="practiceTextRow">
-            <input
+            <textarea
                 className="practiceTextRow--input"
                 value={text}
                 onChange={e => setText(e.target.value)}
                 onBlur={_ => handleSubmission(false)}
                 onKeyDown={e => e.key === 'Enter' ? handleSubmission(true): null}
+                ref={c => textArea = c}
+                rows={1}
                 />
             <div className="practiceTextRow--rating">
                 <RatingScale
@@ -21,7 +27,7 @@ function PracticeTextRow(props) {
                     onRate={props.onRate}/>
             </div>
         </li>
-    )
+    );
 
     function handleSubmission(shouldBlur): void {
         props.onChangeConfirm(text);
