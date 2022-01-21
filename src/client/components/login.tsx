d52e0b225c41c99e683e7ff4df0530569f1a239c
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-01-21 19:00:46 +0530

    Frontend validation of username and password

--- src/client/components/login.tsx
+++ src/client/components/login.tsx
@@ -5,6 +5,9 @@ import { useMutation, gql } from "@apollo/client";
 import { Navigate } from "react-router-dom";
 import { token, UserContext } from "../context";
 
+const USER_VALIDATE_REGEX =/.{1,}/;
+const PASS_VALIDATE_REGEX =/.{1,}/;
+
 const LOGIN_OP = gql`
 mutation Login($username: String!, $password: String!) {
   login(username: $username, password: $password) {
@@ -21,9 +24,11 @@ function Login(){
     const user = useContext(UserContext);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [login, {data, loading, error}] = useMutation(LOGIN_OP);
+    const [login, {data, loading, error: apolloError}] = useMutation(LOGIN_OP);
     const [isTyping, setIsTyping] = useState(false);
     const isLoggedIn = data && data.login && data.login.token;
+    const [uiError, setUiError] = useState("");
+
     if(isLoggedIn) populateUserContext();
     
     return (
@@ -35,19 +40,22 @@ function Login(){
                     <input type="text" placeholder="Username" value={username} onChange={e => handleUsernameChange(e.target.value)}/>
                     <input type="password" placeholder="Password" value={password} onChange={e => handlePasswordChange(e.target.value)}/>
                     <button onClick={handleLogin}>Login</button>
-                    <p className={`error ${error && !isTyping? "visible": "invisible"}`}>
-                        {`${error? error.message :"&nbsp;"}`}
+                    <p className={`error ${(apolloError || uiError) && !isTyping? "visible": "invisible"}`}>
+                        {`${uiError ? uiError :"\u00a0"}`}
+                        {`${!uiError && apolloError? apolloError.message :"\u00a0"}`}
                     </p>
                 </form>
             </section>
     )
 
     function handleUsernameChange(username){
+        setUiError("");
         setIsTyping(true);
         setUsername(username);
     }
 
     function handlePasswordChange(password) {
+        setUiError("");
         setIsTyping(true);
         setPassword(password);
     }
@@ -55,6 +63,16 @@ function Login(){
     function handleLogin(e) {
         e.preventDefault();
         setIsTyping(false);
+
+        if(!USER_VALIDATE_REGEX.test(username)) {
+            setUiError("Please enter username");
+            return;
+        }
+
+        if(!PASS_VALIDATE_REGEX.test(password)) {
+            setUiError("Please enter password");
+            return;
+        }
 
         if(loading) return;
         login({
